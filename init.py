be3bfecdf1d6a97910eebaa5067819dc44152ff0
nelzonlege <nelson.anthony@binghamuni.edu.ng>
2025-07-03 09:48:58 +0100

    1git

--- init.py
+++ init.py
@@ -5,7 +5,6 @@ face_hear_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 while True:
     ret, image = capture.read()  
-
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     faces = face_hear_cascade.detectMultiScale(gray, 1.1, 4)
@@ -16,7 +15,7 @@ while True:
     cv2.imshow("Video", image)
     k = cv2.waitKey(30) & 0xFF
 
-    if k == 27:  # ESC key to exit
+    if k == 27:  
         break
 
 capture.release()
