ab62751fcbee40b4e3b537252a835e1201dc3b8c
nelzonlege <nelson.anthony@binghamuni.edu.ng>
2025-07-03 08:45:25 +0100

    inital commit

---
+++ init.py
@@ -0,0 +1,23 @@
+import cv2
+
+capture = cv2.VideoCapture(0)
+face_hear_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")  
+
+while True:
+    ret, image = capture.read()  
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    faces = face_hear_cascade.detectMultiScale(gray, 1.1, 4)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 5)
+
+    cv2.imshow("Video", image)
+    k = cv2.waitKey(30) & 0xFF
+
+    if k == 27:  # ESC key to exit
+        break
+
+capture.release()
+cv2.destroyAllWindows()
---
+++ main.py
@@ -0,0 +1,17 @@
+import cv2
+
+face_harr_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+image = cv2.imread("boy.jpg")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow("Gray", gray)
+
+faces = face_harr_cascade.detectMultiScale(gray, 1.1, 4)
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 5)
+
+cv2.imshow("Face", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
